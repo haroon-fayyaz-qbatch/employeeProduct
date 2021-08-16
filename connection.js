0b09c8237b13d442e2e7835a56bca6a7ec41e294
haroon-fayyaz-qbatch <haroon.fayyaz@qbatch.com>
2021-08-16 22:12:37 +0500

    Migrations Project Created

---
+++ connection.js
@@ -0,0 +1,20 @@
+require('dotenv').config();
+const config = require('./config/config.json')
+const process = require('process');
+const { Sequelize } = require('sequelize');
+
+
+const sequelize = new Sequelize(config.development.database, config.development.username, config.development.password, {
+    host: config.development.host,
+    dialect: config.development.dialect //* one of 'mysql' | 'mariadb' | 'postgres' | 'mssql' */
+});
+
+const checkConnection = async() => {
+    try {
+        await sequelize.authenticate();
+        console.log('Connection has been established successfully.');
+    } catch (error) {
+        console.error('Unable to connect to the database:', error);
+    }
+}
+module.exports = { sequelize, checkConnection };
