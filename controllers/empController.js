6fefcdbdde5c190c35e81e812faa1fa325fd4b08
haroon-fayyaz-qbatch <haroon.fayyaz@qbatch.com>
2021-08-17 12:20:20 +0500

    Min and Max Salary

--- controllers/empController.js
+++ controllers/empController.js
@@ -27,7 +27,14 @@ const avgSalaryForEachJob = async () => {
     const result = await db["Emp"].findAll({
       attributes: [
         "Job",
-        [db.Sequelize.fn("avg", db.Sequelize.col("Sal")), "avg_sal"],
+        [
+          db.Sequelize.fn(
+            "ROUND",
+            db.Sequelize.fn("avg", db.Sequelize.col("Sal")),
+            2
+          ),
+          "avg_sal",
+        ],
       ],
       //   include: {
       //     model: db["Emp"],
@@ -37,13 +44,43 @@ const avgSalaryForEachJob = async () => {
       group: ["Job"],
       order: [[db.Sequelize.literal("avg_sal"), "DESC"]],
     });
-    console.log(result);
+    for (i in result) {
+      console.log(
+        "Job: ",
+        result[i].dataValues.Job,
+        ", Average Salary:  ",
+        result[i].dataValues.avg_sal
+      );
+    }
+  }
+};
+
+const minAndMaxSalary = async () => {
+  const result = await db["Emp"].findAll({
+    attributes: [
+      "dept_no",
+      [db.Sequelize.fn("min", db.Sequelize.col("Sal")), "minSalary"],
+      [db.Sequelize.fn("max", db.Sequelize.col("Sal")), "maxSalary"],
+    ],
+    group: ["dept_no"],
+    order: [["dept_no"]],
+  });
+  for (let i in result) {
+    console.log(
+      "DeptNo: ",
+      result[i].dataValues.dept_no,
+      "Min Salary: ",
+      result[i].dataValues.minSalary,
+      "Max Salary: ",
+      result[i].dataValues.maxSalary
+    );
   }
 };
 
 const test = async () => {
   await noOfEmployeesInEachDepartment();
   await avgSalaryForEachJob();
+  await minAndMaxSalary();
 };
 
 test();
